095c7549d85ae165b04ba11f70a76665e697bb25
Evgeniya <vremendm@gmail.com>
2018-06-26 21:06:01 +0200

    fix per

--- exercise9/error_rates.py
+++ exercise9/error_rates.py
@@ -8,8 +8,8 @@ def word_error_rate(hyp, ref):
 
     hyp_len = len(hyp)
     ref_len = len(ref)
-    if ref_len==0:
-        return 1.0
+    if ref_len==0 and hyp_len==0:
+        return 0
     distances = numpy.zeros((ref_len+1,hyp_len+1))
     for i in range(ref_len+1):
         for j in range(hyp_len+1):
@@ -32,16 +32,16 @@ def word_error_rate(hyp, ref):
 def per(hyp,ref):
     hyp_len = len(hyp)
     ref_len = len(ref)
-    if ref_len==0:
-        return 1.0
+    if ref_len==0 and hyp_len==0:
+        return 0.0
     ref_counter = Counter(ref)
-    hyp_countr = Counter(hyp)
+    hyp_counter = Counter(hyp)
 
     s = 0
     for w in hyp:
-        s+=abs(ref_counter[w]-hyp_countr[w])
+        s+=abs(ref_counter[w]-hyp_counter[w])
 
-    d_per = 0.5*(abs(ref_len-hyp_len)+s)
+    d_per = s#0.5*(abs(ref_len-hyp_len)+s)
 
 
     return d_per/ref_len
@@ -63,9 +63,16 @@ if __name__=="__main__":
     with open(args.hyp) as rf:
         hypothesis = rf.read()
 
-    hypothesis = [h.split() for h in hypothesis.split("\n")]
-    references = [r.split() for r in references.split("\n")]
+    hypothesis = [h.split() for h in hypothesis.split("\n") if h!=""]
+    references = [r.split() for r in references.split("\n") if r!=""]
+    wers = []
+    pers = []
     for h,r in zip(hypothesis,references):
         e = word_error_rate(h,r)
         p = per(h,r)
+        pers.append(p)
+        wers.append(e)
         print(e,p)
+
+    print(sum(wers)/len(wers))
+    print(sum(pers)/len(pers))
